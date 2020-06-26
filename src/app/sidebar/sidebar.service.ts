969ce3f56b109bcaecc9bcb498c1bb8e2bbc73b5
Neha <nehasinha2012@gmail.com>
2020-06-27 00:41:20 +0530

    forms

--- src/app/sidebar/sidebar.service.ts
+++ src/app/sidebar/sidebar.service.ts
@@ -11,134 +11,49 @@ export class SidebarService {
       title: 'general',
       type: 'header'
     },
+
     {
-      title: 'Dashboard',
-      icon: 'fa fa-tachometer-alt',
+      title: 'Loan Application',
+      icon: 'fas fa-inr',
       active: false,
-      type: 'dropdown',
+      type: 'simple',
+      url: '/app-loan-application',
       badge: {
-        text: 'New ',
-        class: 'badge-warning'
-      },
-      submenus: [
-        {
-          title: 'Dashboard 1',
-          badge: {
-            text: 'Pro ',
-            class: 'badge-success'
-          }
-        },
-        {
-          title: 'Dashboard 2'
-        },
-        {
-          title: 'Dashboard 3'
-        }
-      ]
+        text: '3',
+        class: 'badge-danger'
+      }
+    },{
+      title: 'Loan Information',
+      icon: 'fa fa-inr',
+      active: false,
+      type: 'simple',
+      url: '/app-loan-application',
+      badge: {
+        text: '3',
+        class: 'badge-danger'
+      }
     },
     {
-      title: 'E-commerce',
-      icon: 'fa fa-shopping-cart',
+      title: 'Loan Estimate',
+      icon: 'fa fa-inr',
       active: false,
-      type: 'dropdown',
+      type: 'simple',
+      url: '/app-loan-application',
       badge: {
         text: '3',
         class: 'badge-danger'
       },
-      submenus: [
-        {
-          title: 'Products',
-        },
-        {
-          title: 'Orders'
-        },
-        {
-          title: 'Credit cart'
-        }
-      ]
     },
-    {
-      title: 'Components',
-      icon: 'far fa-gem',
-      active: false,
-      type: 'dropdown',
-      submenus: [
-        {
-          title: 'General',
-        },
-        {
-          title: 'Panels'
-        },
-        {
-          title: 'Tables'
-        },
-        {
-          title: 'Icons'
-        },
-        {
-          title: 'Forms'
-        }
-      ]
-    },
-    {
-      title: 'Charts',
-      icon: 'fa fa-chart-line',
-      active: false,
-      type: 'dropdown',
-      submenus: [
-        {
-          title: 'Pie chart',
-        },
-        {
-          title: 'Line chart'
-        },
-        {
-          title: 'Bar chart'
-        },
-        {
-          title: 'Histogram'
-        }
-      ]
-    },
-    {
-      title: 'Maps',
-      icon: 'fa fa-globe',
-      active: false,
-      type: 'dropdown',
-      submenus: [
-        {
-          title: 'Google maps',
-        },
-        {
-          title: 'Open street map'
-        }
-      ]
-    },
-    {
-      title: 'Extra',
-      type: 'header'
-    },
-    {
-      title: 'Documentation',
-      icon: 'fa fa-book',
+      {
+      title: 'Loan Package',
+      icon: 'fa fa-inr',
       active: false,
       type: 'simple',
+      url: '/app-loan-application',
       badge: {
-        text: 'Beta',
-        class: 'badge-primary'
-      },
-    },
-    {
-      title: 'Calendar',
-      icon: 'fa fa-calendar',
-      active: false,
-      type: 'simple'
-    },
-    {
-      title: 'Examples',
-      icon: 'fa fa-folder',
-      active: false,
-      type: 'simple'
+        text: '3',
+        class: 'badge-danger'
+      }
     }
   ];
   constructor() { }
