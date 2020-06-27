ff9aac530833ae5b681336773df2bc65ed6facd8
Neha <nehasinha2012@gmail.com>
2020-06-27 15:29:09 +0530

    forms

--- src/app/loan-estimate/loan-estimate.component.ts
+++ src/app/loan-estimate/loan-estimate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-loan-estimate',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoanEstimateComponent implements OnInit {
 
+  controls: FormArray;
+  entities = [
+    { id: 1, feeCode: '100', feeAmount: 200, feePaid: 'Borrower',editMode:false },
+    { id: 2, feeCode: '101', feeAmount: 300, feePaid: 'Borrower' ,editMode:false},
+    { id: 3, feeCode: '102', feeAmount: 400, feePaid: 'Lender' ,editMode:false}
+    ];
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
   }
+
+  editForm(entity){
+  	entity.editMode = true;
+  }
+  cancel(entity){
+  	entity.editMode = false;
+  }
+
+  saveEntity(entity, index)
+  {
+  	this.entities[index] = entity;
+  	console.log(entity);
+  	entity.editMode = false;
+  }
+  
 
 }
